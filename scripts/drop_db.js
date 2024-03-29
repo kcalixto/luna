9e570ab051d437d28ef705cdd36c8e09472bfe67
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2024-03-28 23:16:34 -0300

    added a scripts folder to help me :)

---
+++ scripts/drop_db.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: "sa-east-1",
+});
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const tableName = 'mojo-jojo-db';
+
+const scanParams = {
+    TableName: tableName,
+};
+
+const deleteItems = async (items) => {
+    for (const item of items) {
+        const deleteParams = {
+            TableName: tableName,
+            Key: {
+                pk: item.pk,
+                sk: item.sk,
+            },
+        };
+
+        try {
+            await dynamoDB.delete(deleteParams).promise();
+            console.log(`Deleted item with pk: ${item.pk}`);
+        } catch (error) {
+            console.error(`Could not delete item with pk: ${item.pk}. Error: ${error}`);
+        }
+    }
+};
+
+dynamoDB.scan(scanParams, (error, data) => {
+    if (error) {
+        console.error("Unable to scan the table. Error:", JSON.stringify(error, null, 2));
+    } else {
+        console.log("Scan succeeded.");
+        deleteItems(data.Items);
+    }
+});
